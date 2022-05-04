6e265a06622df375f6acf4efba517130640c6fc2
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2022-05-04 16:59:26 +0900

    あーみすったー

--- client/modules/hooks/usePlayerIdStorage.ts
+++ client/modules/hooks/usePlayerIdStorage.ts
@@ -1,16 +1,16 @@
 import { useEffect } from 'react';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValue, useRecoilState } from 'recoil';
 import { nameState, playerIdState, startPlayersState } from '../state';
 
 export const usePlayerIdStorage = () => {
   const playerId = useRecoilValue(playerIdState);
   const players = useRecoilValue(startPlayersState);
-  const setName = useSetRecoilState(nameState);
+  const [name, setName] = useRecoilState(nameState);
 
   useEffect(() => {
     const player = players.find((p) => p.playerId === playerId);
-    if (player !== undefined) {
+    if (player !== undefined && name === '') {
       setName(player.name);
     }
-  }, [players, playerId, setName]);
+  }, [players, playerId, name, setName]);
 };
