45e639e18e2e6177889f76d14e7fb431d318fd8a
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2022-05-04 17:48:21 +0900

    すまほばいばーい

---
+++ client/components/InvalidSP.tsx
@@ -0,0 +1,25 @@
+import { Box, Text } from '@chakra-ui/layout';
+
+const InvalidSP: React.FC = () => {
+  return (
+    <Box
+      backgroundColor="gray.900"
+      width="100%"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      color="white"
+      textAlign="center"
+    >
+      <Text fontSize={64}>🙇‍♀️</Text>
+      <br />
+      スマホは未対応なのだ...
+      <br />
+      すまそん
+    </Box>
+  );
+};
+
+export { InvalidSP };
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,8 +2,16 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { RecoilRoot } from 'recoil';
 import { SocketProvider } from '../components/SocketProvider';
+import { InvalidSP } from '../components/InvalidSP';
 
 export default function App({ Component, pageProps }: AppProps) {
+  if (typeof window !== 'undefined' && window.innerWidth < 990) {
+    return (
+      <ChakraProvider>
+        <InvalidSP />
+      </ChakraProvider>
+    );
+  }
   return (
     <RecoilRoot>
       <ChakraProvider>
