e44466fff499fb4befb515b604bc1c4764389a10
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2022-05-05 09:00:10 +0900

    すめいとふぉーん

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,22 +1,21 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/layout';
 import { RecoilRoot } from 'recoil';
 import { SocketProvider } from '../components/SocketProvider';
 import { InvalidSP } from '../components/InvalidSP';
 
 export default function App({ Component, pageProps }: AppProps) {
-  if (typeof window !== 'undefined' && window.innerWidth < 990) {
-    return (
-      <ChakraProvider>
-        <InvalidSP />
-      </ChakraProvider>
-    );
-  }
   return (
     <RecoilRoot>
       <ChakraProvider>
         <SocketProvider>
-          <Component {...pageProps} />
+          <Box display={{ base: 'block', md: 'none' }}>
+            <InvalidSP />
+          </Box>
+          <Box display={{ base: 'none', md: 'block' }}>
+            <Component {...pageProps} />
+          </Box>
         </SocketProvider>
       </ChakraProvider>
     </RecoilRoot>
