004e679d86812f900f26da20d32c6b7532955387
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2022-05-07 21:35:58 +0900

    お忘れなく

--- client/pages/playing.tsx
+++ client/pages/playing.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Flex, Text, VStack } from '@chakra-ui/layout';
 import { Hand } from '../components/playing/Hand';
 import { GameTable } from '../components/playing/GameTable';
@@ -8,23 +9,28 @@ import { Information } from '../components/playing/Information';
 const Playing: React.FC = () => {
   const round = useRecoilValue(roundState);
   return (
-    <VStack minHeight="100vh" justifyContent="center">
-      <Text
-        position="absolute"
-        color="white"
-        fontWeight="bold"
-        fontSize="4xl"
-        marginBottom="4"
-        top="32px"
-      >
-        第{round}らうんど！勝負だ！
-      </Text>
-      <Flex gridGap="4" alignItems="flex-start">
-        <GameTable />
-        <Information sum />
-      </Flex>
-      <Hand />
-    </VStack>
+    <>
+      <Head>
+        <title>オラオラオラオラァ！</title>
+      </Head>
+      <VStack minHeight="100vh" justifyContent="center">
+        <Text
+          position="absolute"
+          color="white"
+          fontWeight="bold"
+          fontSize="4xl"
+          marginBottom="4"
+          top="32px"
+        >
+          第{round}らうんど！勝負だ！
+        </Text>
+        <Flex gridGap="4" alignItems="flex-start">
+          <GameTable />
+          <Information sum />
+        </Flex>
+        <Hand />
+      </VStack>
+    </>
   );
 };
 
--- client/pages/result.tsx
+++ client/pages/result.tsx
@@ -1,16 +1,22 @@
+import Head from 'next/head';
 import { Text, VStack } from '@chakra-ui/layout';
 import { Actions } from '../components/result/Actions';
 import { Ranking } from '../components/result/Ranking';
 
 const Result: React.FC = () => {
   return (
-    <VStack minHeight="100vh" justifyContent="center">
-      <Text color="white" fontSize="20px" marginBottom="4">
-        今回の結果は...
-      </Text>
-      <Ranking />
-      <Actions />
-    </VStack>
+    <>
+      <Head>
+        <title>けっかはっぴょぉぉぉ～～</title>
+      </Head>
+      <VStack minHeight="100vh" justifyContent="center">
+        <Text color="white" fontSize="20px" marginBottom="4">
+          今回の結果は...
+        </Text>
+        <Ranking />
+        <Actions />
+      </VStack>
+    </>
   );
 };
 
